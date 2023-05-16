67e32cd794eb88e8aec123930b1eb334b14dd395
Izzy @PacificBird <izzymacster@gmail.com>
2023-05-16 14:13:45 -0700

    egg stuff first iteration, age system needs work

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use compute::integrate;
 use rand::prelude::*;
-use std::io::Write;
+use std::{
+    f64::consts::{E, PI},
+    fmt,
+    fmt::{Display, Formatter},
+    io::Write,
+};
 
-const E: f64 = std::f64::consts::E;
+struct DataPoint {
+    pop_captured: u32,
+    pop_0: f64,
+    pop_active: u32,
+    eggs: u32,
+}
+impl Display for DataPoint {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{},{},{},{}",
+            self.pop_captured, self.pop_0, self.pop_active, self.eggs
+        )
+    }
+}
 
 fn main() {
     // let pop_range = 1_000..=100_000;
@@ -14,10 +34,10 @@ fn main() {
     let test_0_csv = test_0
         .into_iter()
         .enumerate()
-        .map(|(idx, x)| format!("{},{},{},{},{}\n", idx, x.0, x.1, x.2, x.3))
+        .map(|(idx, x)| format!("{},{}\n", idx, x.to_string()))
         .reduce(|acc, row| format!("{}{}", acc, row))
         .unwrap();
-    let test_0_csv = format!("dd,captured,population,active,age\n{}", test_0_csv);
+    let test_0_csv = format!("dd,captured,population,active,eggs\n{}", test_0_csv);
     let mut file_0 = std::fs::File::create("test_0.csv").unwrap();
     writeln!(&mut file_0, "{}", test_0_csv).expect("Couldn't write test_0.csv");
 }
@@ -32,27 +52,27 @@ fn simulate(
     prob_detection: f64,
     deg_day_range: std::ops::RangeInclusive<u32>,
     cdf: impl Fn(f64) -> f64,
-) -> Vec<(u32, f64, u32, u32)> {
+) -> Vec<DataPoint> {
     let mut pop_inactive = pop_0 as f64;
+    // pop_inactive *= 1.55;
     let mut pop_active = 0;
     let mut pop_captured = 0;
     let mut avg_age: f64 = 0.0;
-    let mut pop_active_last: u32 = 0;
+    let mut eggs: u32 = 0;
+    let mut mating_pop: u32 = 0;
+    let mut mating_now: u32 = 0;
 
     deg_day_range
         .into_iter()
         .map(|x| {
-            let activated = ((pop_inactive * diff(x as f64, &cdf)) / 100.0).round();
+            // !TODO: Make option for using a PDF directly, or continue using CDF, possibly using an enum?
+            let activated = (pop_inactive * diff(x as f64, &cdf)) / 100.0;
             pop_inactive -= activated;
             pop_active += activated as u32;
+            mating_pop += (activated / 2.0) as u32;
 
-            avg_age += 1f64.min(if pop_active == 0 {
-                0f64
-            } else {
-                (pop_active_last as f64) / (pop_active as f64)
-            });
-
-            let pop_active_before = pop_active;
+            avg_age += 1f64;
+            avg_age *= 1f64 - (activated / pop_active as f64);
 
             let mut rng = thread_rng();
             let (captured, died): (u32, u32) =
@@ -67,23 +87,44 @@ fn simulate(
                         (cap, ded)
                     }
                 });
+
+            mating_now = (mating_pop as f64
+                * integrate::quad5(curried_normal(40.0, 2.0), avg_age - 0.5, avg_age + 0.5))
+            .round() as u32;
+            if x % 3 == 0 {
+                println!(
+                    "{x}, {activated}, {avg_age}, {}",
+                    (1f64 - (activated / (pop_0 as f64 - pop_inactive)))
+                );
+            }
+            mating_pop.checked_sub(mating_now).unwrap_or(0);
+            eggs += (15.0 * mating_now as f64) as u32;
+
             // println!("{},{},{},{}", pop_active, captured, died, avg_age);
             pop_active -= captured;
             pop_captured += captured;
             pop_active = pop_active.checked_sub(died).unwrap_or(0);
 
-            pop_active_last = pop_active;
+            // avg_age *= ((pop_active != 0) as i32) as f64;
 
-            (
+            DataPoint {
                 pop_captured,
-                pop_0 as f64 - pop_inactive,
-                pop_active_before,
-                died,
-            )
+                pop_0: (pop_0 as f64 - pop_inactive),
+                pop_active,
+                eggs,
+            }
         })
-        .collect::<Vec<(u32, f64, u32, u32)>>()
+        .collect::<Vec<DataPoint>>()
 }
 
 fn example_cdf(x: f64) -> f64 {
     100.0 / (1.0 + E.powf(-0.05 * (x - 200.0)))
+}
+
+fn normal(mean: f64, stdev: f64, x: f64) -> f64 {
+    (1.0 / (stdev * (2.0 * PI).sqrt())) * E.powf(-0.5 * ((x - mean) / stdev).powi(2))
+}
+
+fn curried_normal(mean: f64, stdev: f64) -> impl Fn(f64) -> f64 {
+    move |x| normal(mean, stdev, x)
 }
