38c9effc754f84f03446bcb172141c4bb42738d2
Izzy @PacificBird <izzymacster@gmail.com>
2023-05-24 18:48:53 -0700

    Made egg coefficient fitter and added quadratic model

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use compute::integrate;
 use rand::prelude::*;
 use std::{
     f64::consts::{E, PI},
@@ -36,12 +35,20 @@ fn main() {
     // let delta_detection = 0.0005;
     let degree_day_range = 0..=1000;
 
+    // println!("{}", fit_egg_value(0.0, 15.0));
+    // println!("{}", fit_egg_value(40.0, 9.7));
+    // println!("{}", fit_egg_value(50.0, 8.6));
+
+    let mating_delay = 50.0;
     let test_0 = simulate(
-        100000,
+        100_000,
         0.00025,
         degree_day_range.clone(),
         ProbDist::PDF(&jones_wiman_2012),
+        mating_delay,
+        egg_coefficient(mating_delay),
     );
+    // println!("{}", egg_coefficient(mating_delay));
     let test_0_csv = test_0
         .into_iter()
         .enumerate()
@@ -63,18 +70,19 @@ fn simulate(
     prob_detection: f64,
     deg_day_range: std::ops::RangeInclusive<u32>,
     emergence: ProbDist,
+    mating_delay: f64,
+    egg_multiplier: f64,
 ) -> Vec<DataPoint> {
+    let round_correction = 1.5805019683;
     let mut pop_inactive = pop_0 as f64;
-    // pop_inactive *= 1.55;
+    pop_inactive *= round_correction;
     let mut pop_active = 0;
     let mut pop_captured = 0;
     let mut avg_age: f64 = 0.0;
     let mut eggs: u32 = 0;
-    let mut mating_pop: u32 = 0;
-    let mut mating_now: u32 = 0;
     let mut pop_active_last = 0;
 
-    let mating_chance = 0.05;
+    let mating_chance = 0.45;
 
     deg_day_range
         .into_iter()
@@ -90,12 +98,16 @@ fn simulate(
             pop_active += activated as u32;
             // mating_pop += (activated / 2.0) as u32;
 
-            avg_age += 1f64;
-            avg_age *= if pop_active != 0 {
-                (pop_active_last as f64 / pop_active as f64)
-            } else {
-                0.0
-            };
+            avg_age += 1.0
+                * if pop_active != 0 {
+                    pop_active_last as f64 / pop_active as f64
+                } else {
+                    0.0
+                };
+
+            if pop_active == 0 {
+                avg_age *= 0.0;
+            }
 
             let mut rng = thread_rng();
             let (captured, died): (u32, u32) =
@@ -111,28 +123,34 @@ fn simulate(
                     }
                 });
 
-            mating_now = (pop_active as f64 / 2.0) * mating_chance * (1.0 / (1.0 + f64::exp(-0.05 * (x - 0.0))));
-            if x % 5 == 0 {
-                println!(
-                    "{x}, activated: {}, age: {avg_age}, proprtion: {}",
-                    activated.round(),
-                    (1f64 - (activated.round() / pop_active as f64))
-                );
-            }
-            // mating_pop = mating_pop.checked_sub(mating_now).unwrap_or(0);
-            eggs += (15.0 * mating_now as f64) as u32;
+            let delay_steepness = 3.0;
+            let mating_now = ((pop_active as f64 / 2.0)
+                * mating_chance
+                * adjusted_logistic(delay_steepness, mating_delay, avg_age))
+                as u32;
+
+            eggs += (egg_multiplier * mating_now as f64) as u32;
 
             // println!("{},{},{},{}", pop_active, captured, died, avg_age);
             pop_active -= captured;
             pop_captured += captured;
             pop_active = pop_active.checked_sub(died).unwrap_or(0);
 
+            // mating delay debug stuff
+
+            // if x % 5 == 0 {
+            //     println!(
+            //         "{x}, {pop_active}, mating_now: {mating_now}, age: {avg_age}, proprtion: {}",
+            //         pop_active_last as f64 / pop_active as f64
+            //     );
+            // }
+
             pop_active_last = pop_active;
             // avg_age *= ((pop_active != 0) as i32) as f64;
 
             DataPoint {
                 pop_captured,
-                pop_0: (pop_0 as f64 - pop_inactive),
+                pop_0: ((round_correction * pop_0 as f64) - pop_inactive),
                 pop_active,
                 eggs,
             }
@@ -158,10 +176,44 @@ fn jones_wiman_2012(x: f64) -> f64 {
     .max(0.0)
 }
 
-fn normal(mean: f64, stdev: f64, x: f64) -> f64 {
-    (1.0 / (stdev * (2.0 * PI).sqrt())) * E.powf(-0.5 * ((x - mean) / stdev).powi(2))
+// fn normal(mean: f64, stdev: f64, x: f64) -> f64 {
+//     (1.0 / (stdev * (2.0 * PI).sqrt())) * E.powf(-0.5 * ((x - mean) / stdev).powi(2))
+// }
+
+// fn curried_normal(mean: f64, stdev: f64) -> impl Fn(f64) -> f64 {
+//     move |x| normal(mean, stdev, x)
+// }
+
+fn adjusted_logistic(steepness: f64, translation: f64, x: f64) -> f64 {
+    1.0 / (1.0 + f64::exp(-1.0 * steepness * (x as f64 - translation + (2.2 / steepness))))
 }
 
-fn curried_normal(mean: f64, stdev: f64) -> impl Fn(f64) -> f64 {
-    move |x| normal(mean, stdev, x)
+#[allow(dead_code)]
+fn fit_egg_value(delay: f64, target: f64) -> f64 {
+    let mut fit = 1.0;
+    let mut net_repr_rate = 0.0;
+    while (net_repr_rate - target).abs() > 0.01 {
+        let result = simulate(
+            100_000,
+            0.0,
+            0..=1000,
+            ProbDist::PDF(&jones_wiman_2012),
+            delay,
+            fit,
+        )[1000]
+            .eggs
+            .clone();
+
+        let eggs = result as f64;
+        net_repr_rate = eggs / 100_000.0;
+        println!("fit: {fit}, eggs: {eggs}, repr_rate: {net_repr_rate}");
+
+        fit *= target / net_repr_rate;
+    }
+
+    fit
+}
+
+fn egg_coefficient(delay: f64) -> f64 {
+    (0.005147 * delay * delay) - (0.3227 * delay) + 36.02
 }
