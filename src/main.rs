65f1e8b2b630d804ba1b7cdbfb472ea8305996ed
PacificBird <izzymacster@gmail.com>
2023-05-26 11:15:31 -0700

    Making progress on next generations, it's busted rn though

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ struct DataPoint {
     pop_captured: u32,
     pop_0: f64,
     pop_active: u32,
-    eggs: u32,
+    eggs_1: u32,
 }
 impl Display for DataPoint {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(
             f,
             "{},{},{},{}",
-            self.pop_captured, self.pop_0, self.pop_active, self.eggs
+            self.pop_captured, self.pop_0, self.pop_active, self.eggs_1
         )
     }
 }
@@ -44,7 +44,9 @@ fn main() {
         100_000,
         0.00025,
         degree_day_range.clone(),
-        ProbDist::PDF(&jones_wiman_2012),
+        ProbDist::PDF(&jones_wiman_2012_0),
+        ProbDist::PDF(&jones_wiman_2012_1),
+        ProbDist::PDF(&jones_wiman_2012_2),
         mating_delay,
         egg_coefficient(mating_delay),
     );
@@ -70,6 +72,8 @@ fn simulate(
     prob_detection: f64,
     deg_day_range: std::ops::RangeInclusive<u32>,
     emergence: ProbDist,
+    generation_1: ProbDist,
+    generation_2: ProbDist,
     mating_delay: f64,
     egg_multiplier: f64,
 ) -> Vec<DataPoint> {
@@ -79,7 +83,7 @@ fn simulate(
     let mut pop_active = 0;
     let mut pop_captured = 0;
     let mut avg_age: f64 = 0.0;
-    let mut eggs: u32 = 0;
+    let mut eggs_1: u32 = 0;
     let mut pop_active_last = 0;
 
     let mating_chance = 0.45;
@@ -94,8 +98,16 @@ fn simulate(
                 })
                 / 100.0)
                 .max(0.0);
+            let hatched_1 = ((eggs_1 as f64
+                * match generation_1 {
+                    ProbDist::CDF(cdf) => diff(x as f64, &*cdf),
+                    ProbDist::PDF(pdf) => pdf(x as f64),
+                })
+                / 100.0)
+                .max(0.0);
             pop_inactive -= activated;
-            pop_active += activated as u32;
+            eggs_1 -= hatched_1 as u32;
+            pop_active += activated as u32 + hatched_1 as u32;
             // mating_pop += (activated / 2.0) as u32;
 
             avg_age += 1.0
@@ -129,7 +141,7 @@ fn simulate(
                 * adjusted_logistic(delay_steepness, mating_delay, avg_age))
                 as u32;
 
-            eggs += (egg_multiplier * mating_now as f64) as u32;
+            eggs_1 += (egg_multiplier * mating_now as f64) as u32;
 
             // println!("{},{},{},{}", pop_active, captured, died, avg_age);
             pop_active -= captured;
@@ -138,12 +150,12 @@ fn simulate(
 
             // mating delay debug stuff
 
-            // if x % 5 == 0 {
-            //     println!(
-            //         "{x}, {pop_active}, mating_now: {mating_now}, age: {avg_age}, proprtion: {}",
-            //         pop_active_last as f64 / pop_active as f64
-            //     );
-            // }
+            if x % 5 == 0 {
+                println!(
+                    "{x}, {pop_active}, mating_now: {mating_now}, age: {avg_age}, proprtion: {}",
+                    pop_active_last as f64 / pop_active as f64
+                );
+            }
 
             pop_active_last = pop_active;
             // avg_age *= ((pop_active != 0) as i32) as f64;
@@ -152,7 +164,7 @@ fn simulate(
                 pop_captured,
                 pop_0: ((round_correction * pop_0 as f64) - pop_inactive),
                 pop_active,
-                eggs,
+                eggs_1,
             }
         })
         .collect::<Vec<DataPoint>>()
@@ -163,33 +175,36 @@ fn example_cdf(x: f64) -> f64 {
     100.0 / (1.0 + f64::exp(-0.05 * (x - 200.0)))
 }
 
-fn jones_wiman_2012(x: f64) -> f64 {
+fn jones_wiman_2012_0(x: f64) -> f64 {
     let gamma_0 = 1.0737;
     let delta_0 = 1.2349;
     let lambda_0 = 577.2;
     let zeta_0 = 69.0;
     let z_0 = (x - zeta_0) / lambda_0;
-
+    (100.0
+        * (delta_0 / (lambda_0 * (2.0 * PI).sqrt() * z_0 * (1.0 - z_0)))
+        * f64::exp(-0.5 * (gamma_0 + (delta_0 * (z_0 / (1.0 - z_0)).ln())).powi(2)))
+    .max(0.0)
+}
+fn jones_wiman_2012_1(x: f64) -> f64 {
     let gamma_1 = 0.3964;
     let delta_1 = 1.4682;
     let lambda_1 = 825.6;
     let zeta_1 = 494.8;
     let z_1 = (x - zeta_1) / lambda_1;
 
+    (100.0
+        * (delta_1 / (lambda_1 * (2.0 * PI).sqrt() * z_1 * (1.0 - z_1)))
+        * f64::exp(-0.5 * (gamma_1 + (delta_1 * (z_1 / (1.0 - z_1)).ln())).powi(2)))
+    .max(0.0)
+}
+fn jones_wiman_2012_2(x: f64) -> f64 {
     let gamma_2 = 0.0876;
     let delta_2 = 1.0923;
     let lambda_2 = 746.9;
     let zeta_2 = 1101.2;
     let z_2 = (x - zeta_2) / lambda_2;
 
-    (100.0
-        * (delta_0 / (lambda_0 * (2.0 * PI).sqrt() * z_0 * (1.0 - z_0)))
-        * f64::exp(-0.5 * (gamma_0 + (delta_0 * (z_0 / (1.0 - z_0)).ln())).powi(2)))
-    .max(0.0) + 
-    (100.0
-        * (delta_1 / (lambda_1 * (2.0 * PI).sqrt() * z_1 * (1.0 - z_1)))
-        * f64::exp(-0.5 * (gamma_1 + (delta_1 * (z_1 / (1.0 - z_1)).ln())).powi(2)))
-    .max(0.0) + 
     (100.0
         * (delta_2 / (lambda_2 * (2.0 * PI).sqrt() * z_2 * (1.0 - z_2)))
         * f64::exp(-0.5 * (gamma_2 + (delta_2 * (z_2 / (1.0 - z_2)).ln())).powi(2)))
@@ -217,11 +232,13 @@ fn fit_egg_value(delay: f64, target: f64) -> f64 {
             100_000,
             0.0,
             0..=1000,
-            ProbDist::PDF(&jones_wiman_2012),
+            ProbDist::PDF(&jones_wiman_2012_0),
+            ProbDist::PDF(&jones_wiman_2012_1),
+            ProbDist::PDF(&jones_wiman_2012_2),
             delay,
             fit,
         )[1000]
-            .eggs
+            .eggs_1
             .clone();
 
         let eggs = result as f64;
