b8c1c9605c983615b8cec47178f60692b7f532e4
PacificBird <izzymacster@gmail.com>
2023-06-27 12:36:48 -0700

    Got MultiParam implementing IntoIterator

--- src/multisim/mod.rs
+++ src/multisim/mod.rs
@@ -1,5 +1,5 @@
 use super::{simulate, JW_EMERGENCES};
-use crate::DataPoint;
+use crate::{DataPoint, DataPointFrame, ProbDist};
 use std::fmt::{Display, Formatter};
 
 pub struct TaggedDataPoint<const NUM_GEN: usize> {
@@ -20,19 +20,22 @@ impl<const NUM_GEN: usize> TaggedDataPoint<NUM_GEN> {
     }
 
     pub fn new_vec(
-        data: Vec<DataPoint<NUM_GEN>>,
+        data: DataPointFrame<NUM_GEN>,
         pop_0: u32,
         prob_capture: f64,
         mating_delay: f64,
-    ) -> Vec<Self> {
-        data.into_iter()
-            .map(|datapoint| Self {
-                data: datapoint,
-                pop_0,
-                prob_capture,
-                mating_delay,
-            })
-            .collect()
+    ) -> TaggedDataPointFrame<NUM_GEN> {
+        TaggedDataPointFrame(
+            data.0
+                .into_iter()
+                .map(|datapoint| Self {
+                    data: datapoint,
+                    pop_0,
+                    prob_capture,
+                    mating_delay,
+                })
+                .collect(),
+        )
     }
 
     pub fn csv_headers(&self) -> String {
@@ -56,7 +59,7 @@ impl<const NUM_GEN: usize> Display for TaggedDataPoint<NUM_GEN> {
     }
 }
 
-pub struct TaggedDataPointFrame<const NUM_GEN: usize>([TaggedDataPoint<NUM_GEN>]);
+pub struct TaggedDataPointFrame<const NUM_GEN: usize>(pub Vec<TaggedDataPoint<NUM_GEN>>);
 
 impl<const NUM_GEN: usize> TaggedDataPointFrame<NUM_GEN> {
     // pub fn from_untagged_frame()
@@ -68,7 +71,7 @@ impl<const NUM_GEN: usize> TaggedDataPointFrame<NUM_GEN> {
             .csv_headers();
         let data = self
             .0
-            .into_iter()
+            .iter()
             .enumerate()
             .map(|(idx, x)| format!("{},{}\n", idx, x.to_string()))
             .reduce(|acc, row| format!("{}{}", acc, row))
@@ -76,3 +79,35 @@ impl<const NUM_GEN: usize> TaggedDataPointFrame<NUM_GEN> {
         format!("{}\n{}", headers, data)
     }
 }
+
+pub enum MultiParam {
+    Range(std::ops::RangeInclusive<f64>, usize),
+    Constant(f64),
+}
+
+impl std::iter::IntoIterator for MultiParam {
+    type Item = f64;
+    type IntoIter = std::iter::Successors<f64, impl FnMut(&f64) -> Option<f64>>;
+    fn into_iter(self) -> Self::IntoIter {
+        let (start, end, step) = match self {
+            MultiParam::Range(range, step) => (*range.start(), *range.end(), step as f64),
+            MultiParam::Constant(val) => (val, val, 1.0),
+        };
+        std::iter::successors(Some(start), move |prev| {
+            let next = prev + ((end - start) / step);
+            (next < end).then_some(next)
+        })
+    }
+}
+
+// pub fn multisim<const NUM_GEN: usize>(
+//     pop_0: MultiParam,
+//     prob_detection: MultiParam,
+//     deg_day_range: std::ops::RangeInclusive<u32>,
+//     emergences: [ProbDist; NUM_GEN],
+//     mating_delay: MultiParam,
+//     egg_multiplier: impl Fn(f64) -> f64,
+//     steps: u32,
+// ) -> TaggedDataPointFrame<NUM_GEN> {
+//     }
+// }
